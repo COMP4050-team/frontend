e227c602713a73549a3305088fcc1d0882acd540
mqBrian <brian-david-mbalala.wekulo@students.mq.edu.au>
2022-08-23 23:36:01 +1000

    Added testFile page
    Removed the testFile component from the tests page.

--- pages/tests.tsx
+++ pages/tests.tsx
@@ -4,6 +4,7 @@ import { GetTestsDocument } from '../gql/generated/graphql';
 import { CustomList } from '../components/CustomList';
 import { Typography } from '@mui/material';
 import { devFile } from '../devtests/devFile';
+import { TestFile }from './testFile';
 
 const TestsPage: NextPage = () => {
   const [result] = useQuery({
@@ -46,34 +47,6 @@ const TestsPage: NextPage = () => {
       </div>
 
     </>
-  );
-};
-
-const TestFile = ({ studentName, assignment, testsPassed, testsFailed, totalGrade }
-  : {studentName:string; assignment:string; testsPassed: number; testsFailed: number; totalGrade: number}) => {
-  if(!studentName) return <div />;
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            <h5>{studentName}</h5>
-          </td>
-          <td>
-            <h5>{assignment}</h5>
-          </td>
-          <td>
-            <h5>{testsPassed}</h5>
-          </td>
-          <td>
-            <h5>{testsFailed}</h5>
-          </td>
-          <td>
-            <h5>{totalGrade}</h5>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   );
 };
 
