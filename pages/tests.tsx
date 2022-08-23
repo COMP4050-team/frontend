6aa1436aef8571341dfc23920bda587642b7cfd1
mqBrian <brian-david-mbalala.wekulo@students.mq.edu.au>
2022-08-23 22:08:51 +1000

    Simple Additions
    Started working on the tests page,
    Getting a simple table format setup to give specific details of the failed tests.

--- pages/tests.tsx
+++ pages/tests.tsx
@@ -3,6 +3,7 @@ import { useQuery } from 'urql';
 import { GetTestsDocument } from '../gql/generated/graphql';
 import { CustomList } from '../components/CustomList';
 import { Typography } from '@mui/material';
+import { devFile } from '../devtests/devFile';
 
 const TestsPage: NextPage = () => {
   const [result] = useQuery({
@@ -27,7 +28,51 @@ const TestsPage: NextPage = () => {
           }) ?? []
         }
       />
+      <div className="test-container">
+        {devFile.map((data, key) => {
+          return(
+            <div key={key}>
+              <TestFile
+                key={key}
+                studentName={data.studentName}
+                assignment={data.assignment}
+                testsPassed={data.testsPassed}
+                testsFailed={data.testsFailed}
+                totalGrade={data.totalGrade}
+              />
+              </div>
+          );
+        })}
+      </div>
+
     </>
+  );
+};
+
+const TestFile = ({ studentName, assignment, testsPassed, testsFailed, totalGrade }) => {
+  if(!studentName) return <div />;
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <h5>{studentName}</h5>
+          </td>
+          <td>
+            <h5>{assignment}</h5>
+          </td>
+          <td>
+            <h5>{testsPassed}</h5>
+          </td>
+          <td>
+            <h5>{testsFailed}</h5>
+          </td>
+          <td>
+            <h5>{totalGrade}</h5>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
