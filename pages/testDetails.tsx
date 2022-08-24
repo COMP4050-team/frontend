f9d16cf7c787f85dbceb7219059d5242a8741940
mqBrian <brian-david-mbalala.wekulo@students.mq.edu.au>
2022-08-24 23:23:39 +1000

    Did A lot of things
    - Updated yarn
    - Created a new page for the testDetails
    - Created a new page for the data.json for test purposes
    - Updated the table version of what the table could look like instead.

--- pages/testDetails.tsx
+++ pages/testDetails.tsx
@@ -1,74 +1,74 @@
-import { Button } from '@mui/material';
-import { useMemo } from 'react';
+import { Table, Box } from '@mui/material';
+import { DataGrid, GridColDef} from '@mui/x-data-grid';
+import { useState, useEffect } from 'react';
 import {
-    GetObjectCommand,
-    S3Client
+    S3Client,
+    CreateBucketCommand,
+    DeleteBucketCommand,
 } from '@aws-sdk/client-s3';
 import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-provider-cognito-identity';
 
 
-// const command = new GetObjectCommand(input);
-// const response = await client.send(command);
 
-function TestDetails(){
-    // const [getData, setData] = useState([]);
-    // const [template, setTemplate] = useState('Choose Template')
+function testDetails(){
+    const [getData, setData] = useState([]);
+    // const [bucketName, setBucketName] = useState("");
+    // const [successMsg, setSuccessMsg] = useState("");
+    // const [errorMsg, setErrorMsg ] = useState("");
 
-    const REGION = 'ap-southeast-2';
-    const UPLOADS_BUCKET_NAME = 'uploads-76078f4';
+    // const region = 'ap-southeast-2';
+    // const client = new S3Client({
+    //     region,
+    //     credentials: fromCognitoIdentityPool({
+    //         client: new CognitoIdentityClient({ region }),
+    //         identityPoolId:"ap-southeast-2:46ec7d87-6d8a-494f-a5c0-f067f9c45e0b"
+    //     }),
+    // });
 
-    // const client = new S3Client({region: "ap-southeast-2"});
+    // const createBucket = async () => {
+    //     setSuccessMsg("");
+    //     setErrorMsg("");
 
-    const s3 = useMemo(
-        () =>
-          new S3Client({
-            region: REGION,
-            credentials: fromCognitoIdentityPool({
-              client: new CognitoIdentityClient({ region: REGION }),
-              identityPoolId: 'ap-southeast-2:46ec7d87-6d8a-494f-a5c0-f067f9c45e0b', // IDENTITY_POOL_ID
-            }),
-          }),
-      [],
-    );
+    //     try {
+    //         await client.send(new CreateBucketCommand({ Bucket: bucketName }));
+    //         setSuccessMsg(`Bucket "${bucketName}" created.`);
+    //       } catch (e) {
+    //         setErrorMsg(e);
+    //       }
+    //     };
+      
+    //     const deleteBucket = async () => {
+    //       setSuccessMsg("");
+    //       setErrorMsg("");
+      
+    //       try {
+    //         await client.send(new DeleteBucketCommand({ Bucket: bucketName }));
+    //         setSuccessMsg(`Bucket "${bucketName}" deleted.`);
+    //       } catch (e) {
+    //         setErrorMsg(e);
+    //       }
+    // }
 
-    const downloadFile = async () =>{
-
-        // let responseDataChunks = []
-
-        try {          
-            const response = await s3.send(new GetObjectCommand(
-                {
-                  Bucket: UPLOADS_BUCKET_NAME,
-                  Key: "tests/Tests.java" 
-                }))
-
-            console.log(response.ContentType);
-            
-            const reader = new FileReader();
-            // if(response.Body !== undefined){
-            //   let blob = new Blob([response.Body] {type: response.ContentType});
-              
-            //   reader.readAsText(response.Body);
-            // }
-            console.log(typeof reader);
-
-          } catch (err: any) {
-            return alert('There was an error downloading your file: ' + err.message);
-          }
-    }
-
+    useEffect(() => {
+        fetch("../data.json")
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data){
+            setData(data);
+        })
+        .catch(function (err){
+            console.log(err, "error");
+        });
+    }, []);
 
     return (
         <div className="container">
-            <Button  
-              onClick={() => {
-                    downloadFile;}}>
-              Download
-            </Button>
+
         </div>
     )
 
 }
 
-export default TestDetails;
+export default testDetails;
--- pages/tests.tsx
+++ pages/tests.tsx
@@ -4,8 +4,7 @@ import { useQuery } from 'urql';
 import { GetTestsDocument } from '../gql/generated/graphql';
 import { CustomList } from '../components/CustomList';
 import { Typography } from '@mui/material';
-import { devFile } from '../devtests/devFile';
-import { TestFile }from './testFile';
+import {TestFile} from './testFile';
 
 const TestsPage: NextPage = () => {
   const [result] = useQuery({
@@ -31,20 +30,7 @@ const TestsPage: NextPage = () => {
         }
       />
       <div className="test-container">
-        {devFile.map((data, key) => {
-          return(
-            <div key={key}>
-              <TestFile
-                key={key}
-                studentName={data.studentName}
-                assignment={data.assignment}
-                testsPassed={data.testsPassed}
-                testsFailed={data.testsFailed}
-                totalGrade={data.totalGrade}
-              />
-              </div>
-          );
-        })}
+        <TestFile/>
       </div>
 
     </>
