a609494e91e91891f17962616c755b415a4aacc4
mqBrian <brian-david-mbalala.wekulo@students.mq.edu.au>
2022-09-06 19:25:35 +1000

    Update testFile.tsx
    Fixed with prettier

--- pages/testFile.tsx
+++ pages/testFile.tsx
@@ -1,29 +1,28 @@
-import { Box } from '@mui/material';
-import { DataGrid} from "@mui/x-data-grid";
-// import { useState, useEffect } from 'react';
-let Data = require('../data.json');
+import { Box } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+let Data = require("../data.json");
 import TestDetails from "./testDetails";
 
 const columns = Data.columns;
 const rows = Data.rows;
 
-export const TestFile = () =>{
-    return (
-        <Box
-            sx={{
-                height:400,
-                width:'100%'
-            }}
-        >
-            <DataGrid 
-                columns={columns} 
-                rows={rows}
-                pageSize={4}
-                rowsPerPageOptions={[4]} 
-            />
-            <TestDetails />
-        </Box>
-    )
-  };
+export const TestFile = () => {
+  return (
+    <Box
+      sx={{
+        height: 400,
+        width: "100%",
+      }}
+    >
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        pageSize={4}
+        rowsPerPageOptions={[4]}
+      />
+      <TestDetails />
+    </Box>
+  );
+};
 
-  export default TestFile;
+export default TestFile;
